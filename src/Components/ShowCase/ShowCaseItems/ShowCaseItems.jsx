8269028ae06000d111e0fe9321d054b580ea7b89
DimaWoo <DimaWoo2301@gmail.com>
2022-04-09 17:35:01 +0300

    refactoring

--- src/Components/ShowCase/ShowCaseItems/ShowCaseItems.jsx
+++ src/Components/ShowCase/ShowCaseItems/ShowCaseItems.jsx
@@ -4,12 +4,12 @@ import s from "./ShowCaseItems.module.css";
 const ShowCaseItem = (props) => {
   return (
     <div className={s.items}>
-      <span className={s.item}>{props.name}</span>
-      <img className={s.item} src={props.mainImage} alt=""/>
-      <p className={s.item}>{props.text}</p>
+      <span>{props.name}</span>
+      <img src={props.mainImage} alt=""/>
+      <p>{props.text}</p>
       <div className={s.price}>
-        <span className={s.item + " " + s.priceItem}>{props.price}</span>
-        <button className={s.button + " " + s.priceItem}>Купить</button>
+        <span className={s.priceItem}>{props.price}</span>
+        <button className={s.button}>Купить</button>
       </div>
     </div>
   );
